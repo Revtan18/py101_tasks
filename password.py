3a01b78773c4e11ce745c591514d786c3399262f
Revtan18 <yejik14@mail.ru>
2020-11-05 09:08:46 +0100

    Mission Complete!

--- password.py
+++ password.py
@@ -7,4 +7,22 @@
 """
 
 if __name__ == '__main__':
-    pass
+    count = [0,0,0]
+    while count != [1,1,1]:
+        password = input("Введите пароль:")
+        if len(password) < 8:
+            print ("Пароль должен иметь не менее 8 символов")  
+        else:
+            count[0] = 1
+        control = any(i for i in password if i.isupper() == True)
+        if control == False:
+            print("Пароль должен иметь хотя бы одну заглавную букву")
+        else:
+            count[1] = 1
+        control = any(i for i in password if i.isdigit() == True)
+        if control == False:
+            print("Пароль должен иметь хотя бы одну цифру")
+        else:
+            count[2] = 1
+
+    print("Пароль соответствует всем критериям сложности!")
